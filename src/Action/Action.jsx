b5cfa952be80912f04fbea58c664a2dda5e5f3e2
Arjun M L <25b054@nssce.ac.in>
2025-12-26 22:56:51 +0530

    Update Action.jsx

--- src/Action/Action.jsx
+++ src/Action/Action.jsx
@@ -1,6 +1,6 @@
 const Action = () => (
   <div className="flex flex-col justify-center items-center text-center w-screen h-1/3 gap-10">
-    <h1 className="w-[75vw]">Welcome to my site!</h1>
+    <h1 className="w-[75vw]">Arjun Liji - Official Website</h1>
     <p className="w-[90vw]">
       My name is Arjun M Liji, I am 18 currently pursuing my Bachelor's in Computer Science and Engineering at NSSCE, Palakkad.
       Thank you for visiting my site and if you are here just to chill just relax while I build this website further. Thank you for ur patience...
