ba5c44d9ec769de33e908d751a89847da76bdeb5
Arjun M L <25b054@nssce.ac.in>
2025-12-26 12:12:03 +0530

    Update Footer.jsx

--- src/Footer/Footer.jsx
+++ src/Footer/Footer.jsx
@@ -3,7 +3,7 @@ const Footer = () => (
     <div className="flex flex-col items-center gap-1">
       <h1>arjc.me</h1>
       <div className="flex gap-4">
-        <a href="#about">About</a>|<a href="#contact">Legal</a>|<a href="#privacy">Privacy</a>
+        <a href="/about.html">About</a>|<a href="/legal.html">Legal</a>|<a href="/privacy.html">Privacy</a>
       </div>
       <div>&copy; 2023-{new Date().getFullYear()} Arjun M Liji</div>
     </div>
