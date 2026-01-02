4d32f015b5024301edacc114701d734ca8255a0a
Arjun M L <25b054@nssce.ac.in>
2026-01-02 16:33:39 +0530

    Update Footer.jsx

--- src/Footer/Footer.jsx
+++ src/Footer/Footer.jsx
@@ -5,7 +5,10 @@ const Footer = () => (
       <div className="flex gap-4 font-1">
         <a href="/about">About</a>|<a href="/legal">Legal</a>|<a href="/privacy">Privacy</a>
       </div>
-      <div>&copy; 2023-{new Date().getFullYear()} Arjun M Liji</div>
+      <div>&copy; 2023-{new Date().getFullYear()} Arjun M Liji
+<!-- hitwebcounter Code START -->
+<a href="https://www.hitwebcounter.com/" target="_blank">
+<img src="https://hitwebcounter.com/counter/counter.php?page=21466985&style=0001&nbdigits=8&type=page&initCount=0" title="Free Tools" Alt="Free Tools"   border="0" /></a></div>
     </div>
   </footer>
 );
