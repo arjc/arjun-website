0a7a6517ba0783e7ee1f9a0c429e4b2a5f94ffda
Arjun M L <25b054@nssce.ac.in>
2026-01-05 15:07:23 +0530

    Update project.js

--- public/data/project.js
+++ public/data/project.js
@@ -1,15 +1,15 @@
 const project = [
   {
     Name: "Line-cut",
-    description: "Stupid silly match 3 game that I made using vanilla JS.",
-    img: "src/assets/line-cut.webp",
+    description: "A personalized match 3 game, made using vanilla JS.",
+    img: "src/assets/projects/line-cut.webp",
     link: "https://arjc.github.io/line-cut",
     year: "2025",
   },
   {
     Name: "3d counter",
     description: "A program that allows for multidimensional counting.",
-    img: "src/assets/3d-counter.webp",
+    img: "src/assets/projects/3d-counter.webp",
     link: "https://arjc.github.io/3d-counter",
     year: "2025",
   },
@@ -17,7 +17,7 @@ const project = [
     Name: "3D NEOs Orrery",
     description:
       "A 3D orrery simulating NEOs (Near-Earth Objects) solar system. [Nasa hackathon 2025 project]",
-    img: "src/assets/project3.webp",
+    img: "src/assets/projects/neos.webp",
     link: "https://nosebyte.github.io",
     year: "2024",
   },
@@ -25,18 +25,34 @@ const project = [
     Name: "ArchJun - Arjun OS",
     description:
       "Arch Linux based distribution made for ricers and power users. Easily store Terabye worth of homework files, which you can easily ssh from anywhere.",
-    img: "src/assets/archjun.webp",
-    link: "https://arjunliji.me",
+    img: "src/assets/projects/archjun.webp",
+    link: "https://dev.arjc.me/archjun",
     year: "2024",
   },
   {
     Name: "Pavanai Multi Boot uitility",
     description:
-      "Arch Linux based distribution made for ricers and power users. Easily store Terabye worth of homework files, which you can easily ssh from anywhere.",
-    img: "src/assets/archjun.webp",
-    link: "https://arjunliji.me",
+      "Pavanai helps you access plenty of bootable ISOs files stored in his briefcase directly to your pendrive! A ventoy theme for absolute Pavanai stans. A Malappuram Kathi owners dream and passion to see Pavanai each time you fix your computer.",
+    img: "src/assets/projects/pavanai.webp",
+    link: "https://dev.arjc.me/pavanai",
     year: "2024",
   },
+  {
+    Name: "Python ASCII animation",
+    description:
+      "Make kidilam python ASCII animations, displayed on your console.",
+    img: "src/assets/protects/vyali.webp",
+    link: "https://dev.arjc.me/vyali",
+    year: "2024",
+  },
+  {
+    Name: "",
+    description:
+      "Make kidilam python ASCII animations, displayed on your console.",
+    img: "src/assets/protects/vyali.webp",
+    link: "https://dev.arjc.me/vyali",
+    year: "2024",
+  }
 ];
 
 export default project;
