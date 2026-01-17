c7db4c0a3f3e7e9a6ba813718c2a809e3b94fc89
arjcc <25b054@nssce.ac.in>
2026-01-17 15:19:36 +0530

    chanaged typography and increased font size

--- src/Action/Action.jsx
+++ src/Action/Action.jsx
@@ -8,14 +8,14 @@ const Action = ({ parallaxRef }) => {
       parallaxRef.current.scrollTo(page);
     }
   };
-  
+
   return (
     <div className="flex flex-col  items-center text-center w-screen h-[103vh] gap-10 bg-black pb-10">
       <div className="w-screen bg-white overflow-hidden">
-        <span 
+        <span
           className="inline-block font-extrabold text-black px-1 py-3 text-6xl sm:text-8xl leading-normal whitespace-nowrap animate-marquee font-des"
           style={{
-            animation: 'marquee 16s ease-in-out infinite alternate',
+            animation: "marquee 16s ease-in-out infinite alternate",
           }}
         >
           {isMalayalam ? "നമസ്കാരം! ഞാൻ അർജുൻ ലിജി ~ " : "Hello! Arjun Here ~ "}
@@ -34,28 +34,57 @@ const Action = ({ parallaxRef }) => {
           }
         }
       `}</style>
-      <p className="w-[90vw] sm:w-[70vw] text-[2em] opacity-60 font-para my-20">
-        {isMalayalam 
-          ? "നമസ്കാരം! എന്റെ പേര് അർജുൻ എം ലിജി (18), എനിക്ക് സംഗീതം, \nകമ്പ്യൂട്ടർ തുടങ്ങിയവ വളരെയധികം ഇഷ്ടമാണ്. എന്റെ വെബ്‌സൈറ്റിലേക്ക് വന്നതിനു നന്ദി."
-          : `My name is Arjun M Liji, I am a ${new Date().getFullYear() - 2007} year old guy who has a deep love for computers and music. Thank you for visiting my website.`}
+      <p className="w-[90vw] sm:w-[70vw] text-4xl sm:text-5xl opacity-60 font-para my-20">
+        {isMalayalam ? (
+          <div className="tracking-widest">
+            <span>
+              നമസ്കാരം! എന്റെ പേര് അർജുൻ എം ലിജി ({new Date().getFullYear() - 2007}), എനിക്ക് സംഗീതം,
+              കമ്പ്യൂട്ടർ തുടങ്ങിയവ വളരെയധികം ഇഷ്ടമാണ്.
+            </span>
+            <span>എന്റെ വെബ്‌സൈറ്റിലേക്ക് വന്നതിനു നന്ദി.</span>
+          </div>
+        ) : (
+          <>
+            <span>
+              My name is Arjun M Liji I am a {new Date().getFullYear() - 2007}{' '}
+              year old guy who has a deep love for computers and music. 
+            </span>{' '}
+            <span>Thank you for visiting my website.</span>
+          </>
+        )}
       </p>
-      <div className="flex flex-col flex-wrap justify-center gap-5 px-10 sm:flex-row text-xl sm:text-lg font-des">
-        <a href="https://dev.arjc.me" className="px-3 py-1 border-y sm:border-5 sm:rounded-2xl w-screen sm:w-auto ">
+      <div className="flex flex-col flex-wrap sm:flex-row justify-center items-baseline gap-5 text-4xl sm:text-5xl leading-normal font-des">
+        <a
+          href="https://dev.arjc.me"
+          className="px-3 py-1 border-y sm:border-4 sm:rounded-2xl w-screen sm:w-auto "
+        >
           {isMalayalam ? "ഡെവ് പോർട്ട്ഫോളിയോ" : "Dev Portfolio"}
         </a>
-        <a href="https://blogs.arjc.me" className="px-3 py-1 border-y sm:border-5 sm:rounded-2xl w-screen sm:w-auto ">
+        <a
+          href="https://blogs.arjc.me"
+          className="px-3 py-1 border-y sm:border-4 sm:rounded-2xl w-screen sm:w-auto "
+        >
           {isMalayalam ? "പത്രം" : "Blog"}
         </a>
-        <a href="https://music.arjc.me" className="px-3 py-1 border-y sm:border-5 sm:rounded-2xl w-screen sm:w-auto ">
+        <a
+          href="https://music.arjc.me"
+          className="px-3 py-1 border-y sm:border-4 sm:rounded-2xl w-screen sm:w-auto "
+        >
           {isMalayalam ? "സംഗീതകൃതികൾ" : "Music and works"}
         </a>
         {/* <a href="/cv.pdf" className="px-3 py-1 border-y sm:border-5 sm:rounded-2xl w-screen sm:w-auto ">
           {isMalayalam ? "സി.വി" : "Download CV"}
         </a> */}
-        <a onClick={() => scrollTo(2)} className="px-3 py-1 border-y sm:border-5 sm:rounded-2xl w-screen sm:w-auto cursor-pointer">
+        <a
+          onClick={() => scrollTo(2)}
+          className="px-3 py-1 border-y sm:border-4 sm:rounded-2xl w-screen sm:w-auto cursor-pointer"
+        >
           {isMalayalam ? "ചിത്രങ്ങൾ" : "Gallery"}
         </a>
-        <a onClick={() => scrollTo(4)} className="px-3 py-1 border-y sm:border-5 sm:rounded-2xl w-screen sm:w-auto cursor-pointer">
+        <a
+          onClick={() => scrollTo(4)}
+          className="px-3 py-1 border-y sm:border-4 sm:rounded-2xl w-screen sm:w-auto cursor-pointer"
+        >
           {isMalayalam ? "സന്വര്ക്കം" : "Contact"}
         </a>
       </div>
